958ec105995494831fb4438f2dcd819e607ebb30
Maciek <your@email.com>
2019-12-15 11:23:51 +0100

    OK

---
+++ 1_asercje.py
@@ -0,0 +1,16 @@
+# def dzielenie(a,b):
+#     return a/b
+#
+# print(dzielenie(4,2)== 2) # Sprawdzamy od razu odpowiedż
+#
+# if dzielenie(4,2)== 2:
+#     print("Passed")
+# else:
+#     print("Failed")
+#
+# if dzielenie(5,10)== 0.5:
+#     print("Passed")
+# else:
+#     print("Failed")
+
+
